fa9d9e678b2d42dafb830a7b7eda915a0c1b84d7
UVE-JCALLEJA\Jesus Calleja <jesus.calleja@uvesolutions.com>
2022-02-15 00:49:11 +0100

    WIP - Scroll snap fixed

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ window.onload = onResize;
 window.onresize = onResize;
 
 function onResize(){
-    var elements = document.getElementsByClassName('full-height');
+    var wrapper = document.getElementById('wrapper');
+    var elements = document.getElementsByClassName('full-screen-section');
+    const height = `${window.innerHeight}px`;
+    
+    wrapper.style.height = height;
     Array.prototype.filter.call(elements, function(e){
-        e.style.height = `${window.innerHeight}px`;
+        e.style.height = height;
     });
 }
