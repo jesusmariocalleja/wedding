532c567e21097c6285a15247dbfd2ea770068724
UVE-JCALLEJA\Jesus Calleja <jesus.calleja@uvesolutions.com>
2022-02-15 01:35:30 +0100

    WIP - Added 💩 css animations

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ window.onload = onResize;
 window.onresize = onResize;
 
 function onResize(){
-    var wrapper = document.getElementById('wrapper');
-    var elements = document.getElementsByClassName('full-screen-section');
     const height = `${window.innerHeight}px`;
     
+    var wrapper = document.getElementById('wrapper'); 
     wrapper.style.height = height;
+    
+    var elements = document.getElementsByClassName('full-screen-section');
     Array.prototype.filter.call(elements, function(e){
         e.style.height = height;
     });
