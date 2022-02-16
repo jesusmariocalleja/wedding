c4e7019ee9df126c8772bc85f88f58adcf2e98ae
UVE-JCALLEJA\Jesus Calleja <jesus.calleja@uvesolutions.com>
2022-02-16 20:16:41 +0100

    EVO Countdown restyle + Header leafs not cut by overflow

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -2,18 +2,18 @@ countdown();
 
 function countdown(){
     const targetTime = moment('2022-11-05 12:00');
-    var element = document.getElementById('countdown'); 
     
-    printTime(element, targetTime);
+    printTime(targetTime);
     setInterval(function(){
-        printTime(element, targetTime);
+        printTime(targetTime);
     }, 1000);
 }
 
-function printTime(element, targetTime){
+function printTime(targetTime){
     const timeBetween = moment.duration(targetTime.diff(moment()));
-    // Días
-    element.innerText = `${parseInt(timeBetween.asDays())} días, ${timeBetween.hours()} horas, ${timeBetween.minutes()} minutos y ${timeBetween.seconds()} segundos`;
-    // Meses y días
-    //element.innerText = `${timeBetween.months()} meses, ${timeBetween.days()} días, ${timeBetween.hours()} horas, ${timeBetween.minutes()} minutos y ${timeBetween.seconds()} segundos`;
+    
+    document.getElementById('countdown-days').innerText = parseInt(timeBetween.asDays());
+    document.getElementById('countdown-hours').innerText = timeBetween.hours();
+    document.getElementById('countdown-minutes').innerText = timeBetween.minutes();
+    document.getElementById('countdown-seconds').innerText = timeBetween.seconds();
 }
