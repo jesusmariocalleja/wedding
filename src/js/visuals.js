985628f22d25fc88eb04b78641c1640c8f724fdc
UVE-JCALLEJA\Jesus Calleja <jesus.calleja@uvesolutions.com>
2022-02-20 14:36:02 +0100

    EVO Fade In on scroll appear

--- src/js/visuals.js
+++ src/js/visuals.js
@@ -1,5 +1,14 @@
-window.onload = onResize;
-window.onresize = onResize;
+window.addEventListener('load', scrollTo);
+window.addEventListener('load', onResize);
+window.addEventListener('load', appearOnScroll);
+window.addEventListener('resize', onResize);
+
+function scrollTo(){
+    var wrapper = document.getElementById('wrapper'); 
+    setTimeout(() => {
+        wrapper.scrollTop = 0;
+    }, 0)
+}
 
 function onResize(){
     const height = `${window.innerHeight}px`;
@@ -11,4 +20,25 @@ function onResize(){
     Array.prototype.filter.call(elements, function(e){
         e.style.minHeight = height;
     });
+}
+
+function appearOnScroll(){
+    const intersectionScrollOpts = {
+        threshold: 1
+    };
+    const intersectionScrollObserver = new IntersectionObserver((entries, intersectionScrollObserver) => {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            } else{
+                entry.target.classList.add('fade');
+                intersectionScrollObserver.unobserve(entry.target);
+            }
+        })
+    }, intersectionScrollOpts);
+
+    const sections = document.querySelectorAll('.full-screen-section');
+    sections.forEach(section => {
+        intersectionScrollObserver.observe(section);
+    })
 }
