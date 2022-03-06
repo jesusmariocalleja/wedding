a9d58cfddd41c5cb5b00940fe3fb9973c6aeb92f
UVE-JCALLEJA\Jesus Calleja <jesus.calleja@uvesolutions.com>
2022-03-06 22:41:34 +0100

    Fix

--- src/js/clipboard.js
+++ src/js/clipboard.js
@@ -5,7 +5,7 @@ function copyBankAccountToClipboard(){
     navigator.clipboard.writeText('ES5315632626373261283080');
     let button = document.getElementById("copy-bank-account-button");
     const prevHtml = button.innerHTML;
-    button.innerHTML = '¡Copiado!'
+    button.innerHTML = '¡Copiado!';
 
     setTimeout(function(){
         button.innerHTML = prevHtml;
