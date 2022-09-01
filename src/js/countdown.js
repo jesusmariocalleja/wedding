e553790d1b7498c716027c0d33c6657de5033c16
Jesús Calleja <jesus.calleja@uvesolutions.com>
2022-09-01 20:18:52 +0200

    EVO - Updated timetable

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,19 +1,23 @@
 countdown();
 
-function countdown(){
-    const targetTime = moment('2022-11-05 12:00');
-    
+function countdown() {
+  const targetTime = moment("2022-11-05 13:00");
+
+  printTime(targetTime);
+  setInterval(function () {
     printTime(targetTime);
-    setInterval(function(){
-        printTime(targetTime);
-    }, 1000);
+  }, 1000);
 }
 
-function printTime(targetTime){
-    const timeBetween = moment.duration(targetTime.diff(moment()));
-    
-    document.getElementById('countdown-days').innerText = parseInt(timeBetween.asDays());
-    document.getElementById('countdown-hours').innerText = timeBetween.hours();
-    document.getElementById('countdown-minutes').innerText = timeBetween.minutes();
-    document.getElementById('countdown-seconds').innerText = timeBetween.seconds();
+function printTime(targetTime) {
+  const timeBetween = moment.duration(targetTime.diff(moment()));
+
+  document.getElementById("countdown-days").innerText = parseInt(
+    timeBetween.asDays()
+  );
+  document.getElementById("countdown-hours").innerText = timeBetween.hours();
+  document.getElementById("countdown-minutes").innerText =
+    timeBetween.minutes();
+  document.getElementById("countdown-seconds").innerText =
+    timeBetween.seconds();
 }
--- src/js/visuals.js
+++ src/js/visuals.js
@@ -1,44 +1,47 @@
-window.addEventListener('load', scrollTo);
-window.addEventListener('load', onResize);
-window.addEventListener('load', appearOnScroll);
-window.addEventListener('resize', onResize);
+window.addEventListener("load", scrollTo);
+window.addEventListener("load", onResize);
+window.addEventListener("load", appearOnScroll);
+window.addEventListener("resize", onResize);
 
-function scrollTo(){
-    var wrapper = document.getElementById('wrapper'); 
-    setTimeout(() => {
-        wrapper.scrollTop = 0;
-    }, 0)
+function scrollTo() {
+  var wrapper = document.getElementById("wrapper");
+  setTimeout(() => {
+    wrapper.scrollTop = 0;
+  }, 0);
 }
 
-function onResize(){
-    const height = `${window.innerHeight}px`;
-    
-    var wrapper = document.getElementById('wrapper'); 
-    wrapper.style.height = height;
-    
-    var elements = document.getElementsByClassName('full-screen-section');
-    Array.prototype.filter.call(elements, function(e){
-        e.style.minHeight = height;
-    });
+function onResize() {
+  const height = `${window.innerHeight}px`;
+
+  var wrapper = document.getElementById("wrapper");
+  wrapper.style.height = height;
+
+  var elements = document.getElementsByClassName("full-screen-section");
+  Array.prototype.filter.call(elements, function (e) {
+    e.style.minHeight = height;
+  });
 }
 
-function appearOnScroll(){
-    const intersectionScrollOpts = {
-        threshold: 0.3 //0.5
-    };
-    const intersectionScrollObserver = new IntersectionObserver((entries, intersectionScrollObserver) => {
-        entries.forEach(entry => {
-            if(!entry.isIntersecting){
-                return;
-            } else{
-                entry.target.classList.add('fade');
-                intersectionScrollObserver.unobserve(entry.target);
-            }
-        })
-    }, intersectionScrollOpts);
+function appearOnScroll() {
+  const intersectionScrollOpts = {
+    threshold: 0.3, //0.5
+  };
+  const intersectionScrollObserver = new IntersectionObserver(
+    (entries, intersectionScrollObserver) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add("fade");
+          intersectionScrollObserver.unobserve(entry.target);
+        }
+      });
+    },
+    intersectionScrollOpts
+  );
 
-    const sections = document.querySelectorAll('.appear-on-scroll');
-    sections.forEach(section => {
-        intersectionScrollObserver.observe(section);
-    })
+  const sections = document.querySelectorAll(".appear-on-scroll");
+  sections.forEach((section) => {
+    intersectionScrollObserver.observe(section);
+  });
 }
